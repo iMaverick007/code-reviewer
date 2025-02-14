d1091c15b582167cf420d6d57c325b6421974791
iMaverick007 <alloydfernandes007@gmail.com>
2025-02-14 12:40:19 +0530

    updates

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env': {}
+  build: {
+    outDir: 'build'  // Change the output directory to build
   }
 });
